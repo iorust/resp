e53eb3ae94800905df7bb2b1cafeccfedd3f2717
zensh <admin@zensh.com>
2016-03-03 08:30:08 +0800

    update lib doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+#![warn(missing_docs)]
 #![doc(html_logo_url = "https://avatars3.githubusercontent.com/u/15439811?v=3&s=200",
        html_favicon_url = "https://iorust.github.io/favicon.ico",
-       html_root_url = "https://iorust.github.io/resp/resp")]
+       html_root_url = "https://iorust.github.io",
+       html_playground_url = "https://play.rust-lang.org",
+       issue_tracker_base_url = "https://github.com/iorust/resp/issues")]
 
 //! RESP(REdis Serialization Protocol) Serialization for Rust.
 
